4bdcafca23a513b9ecd5ff493b3c006f8bf69b92
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-06-20 10:45:51 +0200

    Create Hamburger.js

---
+++ src/components/Hamburger.js
@@ -0,0 +1,143 @@
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { Link } from "react-router-dom";
+
+import {
+  staggerText,
+  staggerReveal,
+  fadeInUp,
+  handleHover,
+  handleHoverExit,
+  handleCityReturn,
+  handleCity,
+  staggerRevealClose
+} from "./Animations";
+
+import code from "../images/code.webp";
+import finance from "../images/finance.webp";
+import hockey from "../images/hockey.webp";
+import music from "../images/music.webp";
+
+const cities = [
+  { name: "Code", image: code },
+  { name: "Stock", image: finance },
+  { name: "Hockey", image: hockey },
+  { name: "Music", image: music },
+];
+
+const Hamburger = ({ state }) => {
+  // Create varibles of our dom nodes
+  let menuLayer = useRef(null);
+  let reveal1 = useRef(null);
+  let reveal2 = useRef(null);
+  let cityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let info = useRef(null);
+
+  useEffect(() => {
+    // If the menu is open and we click the menu button to close it.
+    if (state.clicked === false) {
+      // If menu is closed and we want to open it.
+
+      staggerRevealClose(reveal2, reveal1);
+      // Set menu to display none
+      gsap.to(menuLayer, { duration: 1, css: { display: "none" } });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      // Set menu to display block
+      gsap.to(menuLayer, { duration: 0, css: { display: "block" } });
+      //Allow menu to have height of 100%
+      gsap.to([reveal1, reveal2], {
+        duration: 0,
+        opacity: 1,
+        height: "100%"
+      });
+      staggerReveal(reveal1, reveal2);
+      fadeInUp(info);
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
+
+  return (
+    <div ref={el => (menuLayer = el)} className='hamburger-menu'>
+      <div
+        ref={el => (reveal1 = el)}
+        className='menu-secondary-background-color'></div>
+      <div ref={el => (reveal2 = el)} className='menu-layer'>
+        <div
+          ref={el => (cityBackground = el)}
+          className='menu-city-background'></div>
+        <div className='container'>
+          <div className='wrapper'>
+            <div className='menu-links'>
+              <nav>
+                <ul>
+                  <li>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line1 = el)}
+                      to='/projects'>
+                      PROJECTS
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line2 = el)}
+                      to='/cv'>
+                      CV
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      onMouseEnter={e => handleHover(e)}
+                      onMouseOut={e => handleHoverExit(e)}
+                      ref={el => (line3 = el)}
+                      to='/contact'>
+                      CONTACT
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <div ref={el => (info = el)} className='info'>
+                <h3 style={{color: "black"}}>GOAL</h3>
+                <p style={{color: "black"}}>
+                  Fullstack-developer with just as good knowledge about
+                  both the frontend, as well as the backend.
+                  At the moment im just a frontend guy, but 
+                  learning more and more about backend every day.
+                </p>
+                <br></br>
+                <p style={{color: "black"}}>
+                  P.S.
+                  This website is not responsive, so dont even try ;)
+                </p>
+              </div>
+              <div className='locations'>
+                My life:
+                {/* Returning the list of cities */}
+                {cities.map(el => (
+                  <span
+                    style={{color: "black"}}
+                    key={el.name}
+                    onMouseEnter={() => handleCity(el.image, cityBackground)}
+                    onMouseOut={() => handleCityReturn(cityBackground)}>
+                    {el.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
